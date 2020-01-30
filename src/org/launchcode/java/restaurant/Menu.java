c11b2bd78577180dbc185764b44a36d72e6faca0
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2020-01-29 18:28:31 -0600

    forgot to add menuItem as a field for the Menu class

--- src/org/launchcode/java/restaurant/Menu.java
+++ src/org/launchcode/java/restaurant/Menu.java
@@ -4,9 +4,11 @@ import java.util.Date;
 
 public class Menu {
     private Date lastUpdate;
+    private MenuItem newMenuItem;
 
-    public Menu (Date lastUpdate){
+    public Menu (Date lastUpdate, MenuItem newMenuItem){
         this.lastUpdate = lastUpdate;
+        this.newMenuItem = newMenuItem;
     }
 
     public Date getLastUpdate(){
@@ -15,5 +17,13 @@ public class Menu {
 
     public void setLastUpdate(Date lastUpdate){
         this.lastUpdate = lastUpdate;
+    }
+
+    public MenuItem getNewMenuItem(){
+        return newMenuItem;
+    }
+
+    public void setNewMenuItem (MenuItem newMenuItem){
+        this.newMenuItem = newMenuItem;
     }
 }
